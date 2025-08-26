0e6dba9afcab47cf1002f0a7cdd6b88008b62915
CristinaJung <cristijung@gmail.com>
2025-08-26 14:21:45 -0300

    estrutura da aplicação + rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Home } from './page/home/home';
+import { Sobre } from './page/sobre/sobre';
+import { Servicos } from './page/servicos/servicos';
+import { Contato } from './page/contato/contato';
+import { NotFound } from './page/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full',},
+    { path: 'home', component: Home },
+    { path: 'sobre', component: Sobre },
+    { path: 'servicos', component: Servicos },
+    { path: 'contato', component: Contato },
+    { path: '**', component: NotFound },
+
+];
