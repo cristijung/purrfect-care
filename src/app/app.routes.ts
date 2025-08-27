9faafc7e683ca6a845ef7ddc03f32d89a018597d
CristinaJung <cristijung@gmail.com>
2025-08-27 14:14:15 -0300

    rota add vet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Sobre } from './page/sobre/sobre';
 import { Servicos } from './page/servicos/servicos';
 import { Contato } from './page/contato/contato';
 import { NotFound } from './page/not-found/not-found';
+import { Saude } from './page/saude/saude';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full',},
     { path: 'home', component: Home },
     { path: 'sobre', component: Sobre },
     { path: 'servicos', component: Servicos },
+    { path: 'saude', component: Saude },
     { path: 'contato', component: Contato },
     { path: '**', component: NotFound },
 
